de3ac9cbf23bd858cd5bcfe9e15e4c7ebfeffa00
sam-dp <cloudysaga@gmail.com>
2023-07-03 01:50:57 -0700

    Built Functionality

--- js.js
+++ js.js
@@ -3,6 +3,7 @@ const DEFAULT_MODE = 'color' // Start in coloring mode
 const DEFAULT_SIZE = 50; // Start with 50x50 dimensions
 
 
+
 let currentColor = DEFUALT_COLOR;
 let currentMode = DEFAULT_MODE;
 let currentSize = DEFAULT_SIZE;
@@ -22,11 +23,22 @@ function setSize(size) {
 
 const colorBtn = document.querySelector('#color-btn');
 const pickerBtn = document.querySelector('#picker-btn');
-const randomBtn = document.querySelector('#randmon-btn');
+const randomBtn = document.querySelector('#random-btn');
 const eraserBtn = document.querySelector('#eraser-btn');
 const clearBtn = document.querySelector('#clear-btn');
+const gridSize = document.querySelector('#grid-size');
 const gridSlider = document.querySelector('#grid-range');
 const grid = document.querySelector('#grid-container');
+
+
+gridSlider.oninput = function() {
+    setGridSize(this.value);
+    updateGridSize(this.value);
+}
+
+function updateGridSize(size) {
+    gridSize.innerHTML = `${size} x ${size}`;
+}
 
 colorBtn.onclick = () => setMode('color');
 pickerBtn.oninput = (e) => setColor(e.target.value);
@@ -34,19 +46,55 @@ randomBtn.onclick = () => setMode('random');
 eraserBtn.onclick = () => setMode('eraser');
 clearBtn.onclick = () => reloadGrid();
 
-
+function setGridSize(size) {
+    setSize(size);
+    reloadGrid();
+}
 
 function reloadGrid() {
-    grid.innerHTML = '';
+    clearGrid();
     buildGrid(currentSize);
 }
 
+function clearGrid() {
+    grid.innerHTML = '';
+}
 
+function buildGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement('div')
+    gridElement.classList.add('grid-element')
+    
+    gridElement.addEventListener('mouseover', changeColor)
+    gridElement.addEventListener('mousedown', changeColor)
 
+    grid.appendChild(gridElement)
+  }
 
+}
 
-// window.onload = () => {
-//     buildGrid(DEFAULT_SIZE)
-//     activateButton(DEFAULT_MODE)
-// }
+function changeColor(e) {
+    if(!e.buttons > 0) return;
+    if (currentMode === 'random') {
+      const randomR = Math.floor(Math.random() * 256)
+      const randomG = Math.floor(Math.random() * 256)
+      const randomB = Math.floor(Math.random() * 256)
+      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'color') {
+      e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'eraser') {
+      e.target.style.backgroundColor = '#FFFFFF'
+    }
+  }
+
+window.onload = () => {
+    updateGridSize(DEFAULT_SIZE);
+    gridSlider.value = DEFAULT_SIZE;
+
+    pickerBtn.value = DEFUALT_COLOR;
+
+    buildGrid(DEFAULT_SIZE);
+}
